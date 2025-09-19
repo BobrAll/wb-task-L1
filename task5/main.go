ce06aca12c89e5cb1662755ecf9a6529b1cf3c43
bobr <bobrus91@gmail.com>
2025-09-19 16:47:23 +0800

    Solved task 5

--- task5/main.go
+++ task5/main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 	"time"
 )
 
+const DefaultWorkingSeconds = 3
+
+// main sends numbers to the channel until the timer expires.
 func main() {
 	numCh := make(chan int)
-	timer := time.After(3 * time.Second)
+	stopTimer := time.After(getWorkingTime())
 	go printNums(numCh)
 
 	for i := 0; ; i++ {
 		select {
-		case <-timer:
+		case <-stopTimer:
 			close(numCh)
 			return
 		case numCh <- i:
@@ -20,6 +26,30 @@ func main() {
 	}
 }
 
+// getWorkingTime returns the duration from command line or default.
+func getWorkingTime() time.Duration {
+	var seconds int
+	if len(os.Args) < 2 {
+		log.Printf("Program was started without specified seconds of working time. Set default value - %d sec."+
+			"\nIf you want to specify - run using 'go run task5/main.go {seconds}'", DefaultWorkingSeconds)
+		seconds = DefaultWorkingSeconds
+	} else {
+		seconds = parseSeconds(os.Args[1])
+	}
+	return time.Duration(seconds) * time.Second
+}
+
+// parseSeconds returns seconds from console argument as num
+func parseSeconds(s string) int {
+	seconds, err := strconv.Atoi(s)
+	if err != nil || seconds < 0 {
+		log.Printf("Invalid input, seconds must be a positive num. Set default value - %d sec.\n", DefaultWorkingSeconds)
+		seconds = DefaultWorkingSeconds
+	}
+	return seconds
+}
+
+// printNums reads numbers from the channel and prints them until the channel is closed.
 func printNums(numCh <-chan int) {
 	for num := range numCh {
 		fmt.Println(num)
